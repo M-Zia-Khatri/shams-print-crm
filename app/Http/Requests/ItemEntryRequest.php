d86379d452fe8382c8c6d20b35a45fc11c9d9067
M-Zia-Khatri <kb313892@gmail.com>
2026-07-24 17:15:08 +0500

    feat: add production Dockerfile and ItemEntryRequest validation logic

--- app/Http/Requests/ItemEntryRequest.php
+++ app/Http/Requests/ItemEntryRequest.php
@@ -26,7 +26,7 @@ class ItemEntryRequest extends FormRequest
                 'entries.*.darjan' => ['required', 'integer', 'min:0'],
                 'entries.*.total_color' => ['required', 'integer', 'min:0'],
                 'entries.*.total_rate' => ['required', 'numeric', 'min:0'],
-                'entries.*.image' => ['required', 'image', 'mimes:jpg,jpeg,png,webp', 'max:5120'],
+                'entries.*.image' => ['required', 'image', 'mimes:jpg,jpeg,png,webp', 'max:20480'],
                 'entries.*.total_amount' => ['prohibited'],
             ];
         }
@@ -39,7 +39,7 @@ class ItemEntryRequest extends FormRequest
             'darjan' => ['required', 'integer', 'min:0'],
             'total_color' => ['required', 'integer', 'min:0'],
             'total_rate' => ['required', 'numeric', 'min:0'],
-            'image' => [($this->isMethod('put') || $this->isMethod('patch')) ? 'nullable' : 'required', 'image', 'mimes:jpg,jpeg,png,webp', 'max:5120'],
+            'image' => [($this->isMethod('put') || $this->isMethod('patch')) ? 'nullable' : 'required', 'image', 'mimes:jpg,jpeg,png,webp', 'max:20480'],
             'total_amount' => ['prohibited'],
         ];
     }
